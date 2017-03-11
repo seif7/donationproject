38a3cfc7b34c32077e4bef0f966585fc014ed385
seif7 <seif.abdelkefi@gmail.com>
2017-03-11 23:16:36 +0100

    Add files via upload

---
+++ Idonate2.0/Idonate.js
@@ -0,0 +1,167 @@
+function checkdone() {
+
+
+    if (document.getElementById("Food").checked == true) {
+
+        document.getElementById("food").src = "toysa.png";
+
+    }
+
+
+
+    // if (document.getElementById("Food").checked == true) {
+
+    //     document.getElementById("food").src = "toysa.png";
+
+    // }
+
+
+
+
+
+    // if (document.getElementById("Food").checked == true) {
+
+    //     document.getElementById("food").src = "toysa.png";
+
+    // }
+
+
+
+
+    // if (document.getElementById("Food").checked == true) {
+
+    //     document.getElementById("food").src = "toysa.png";
+
+    // }
+
+
+
+
+
+    // if (document.getElementById("Food").checked == true) {
+
+    //     document.getElementById("food").src = "toysa.png";
+
+    // }
+
+
+
+    // if (document.getElementById("Food").checked == true) {
+
+    //     document.getElementById("food").src = "toysa.png";
+
+    // }
+
+
+
+
+    if (document.getElementById("Any").checked == false &&
+        document.getElementById("Clothes").checked == false &&
+        document.getElementById("Money").checked == false &&
+        document.getElementById("Toys").checked == false &&
+        document.getElementById("Books").checked == false &&
+        document.getElementById("Food").checked == false &&
+
+        document.getElementById("Blankets").checked == false
+    ) {
+        alert('Please choose at least one option');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function checkdone2() {
+    if (document.getElementById("any2").checked == true) {
+        document.getElementById('mapimage').classList.add("checked");
+    }
+
+    if (document.getElementById("tunis").checked == true) {
+        document.getElementById('tunisid').classList.add("checked");
+    }
+    if (document.getElementById("sfax").checked == true) {
+        document.getElementById('sfaxid').classList.add("checked");
+    }
+    if (document.getElementById("nabeul").checked == true) {
+        document.getElementById('nabeulid').classList.add("checked");
+    }
+    if (document.getElementById("sousse").checked == true) {
+        document.getElementById('sousseid').classList.add("checked");
+    }
+    if (document.getElementById("benarous").checked == true) {
+        document.getElementById('benarousid').classList.add("checked");
+    }
+    if (document.getElementById("tunis").checked == true) {
+        document.getElementById('tunisid').classList.add("checked");
+    }
+    if (document.getElementById("ariana").checked == true) {
+        document.getElementById('arianaid').classList.add("checked");
+    }
+    if (document.getElementById("kairouane").checked == true) {
+        document.getElementById('kairouaneid').classList.add("checked");
+    }
+    if (document.getElementById("bizerte").checked == true) {
+        document.getElementById('bizerteid').classList.add("checked");
+    }
+    if (document.getElementById("mounastir").checked == true) {
+        document.getElementById('mounastirid').classList.add("checked");
+    }
+    if (document.getElementById("medenine").checked == true) {
+        document.getElementById('medenineid').classList.add("checked");
+    }
+    if (document.getElementById("kasserine").checked == true) {
+        document.getElementById('kasserineid').classList.add("checked");
+    }
+    if (document.getElementById("sidibouzid").checked == true) {
+        document.getElementById('sidibouzidid').classList.add("checked");
+    }
+    if (document.getElementById("tunis").checked == true) {
+        document.getElementById('tunisid').classList.add("checked");
+    }
+    if (document.getElementById("tunis").checked == true) {
+        document.getElementById('tunisid').classList.add("checked");
+    }
+    if (document.getElementById("tunis").checked == true) {
+        document.getElementById('tunisid').classList.add("checked");
+    }
+    if (document.getElementById("tunis").checked == true) {
+        document.getElementById('tunisid').classList.add("checked");
+    }
+    if (document.getElementById("tunis").checked == true) {
+        document.getElementById('tunisid').classList.add("checked");
+    }
+    if (document.getElementById("tunis").checked == true) {
+        document.getElementById('tunisid').classList.add("checked");
+    }
+    if (document.getElementById("tunis").checked == true) {
+        document.getElementById('tunisid').classList.add("checked");
+    }
+    if (document.getElementById("tunis").checked == true) {
+        document.getElementById('tunisid').classList.add("checked");
+    }
+    if (document.getElementById("jendouba").checked == true) {
+        document.getElementById('jendoubaid').classList.add("checked");
+    }
+    if (document.getElementById("tunis").checked == true) {
+        document.getElementById('tunisid').classList.add("checked");
+    }
+    if (document.getElementById("tunis").checked == true) {
+        document.getElementById('tunisid').classList.add("checked");
+    }
+
+}
